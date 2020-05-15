1d4140aa827c9a28be9c7b5e2d2e7daf54ecd0e8
rishika06 <rishika2306@gmail.com>
2020-05-15 21:48:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div>The Crown Clothing Project</div>;
+  }
+}
+
+export default App;

acec8ba49a6a66ec82068f28353611e9a3440485
rishika06 <rishika2306@gmail.com>
2020-05-18 12:20:40 +0530

    collection item and preview components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+
 import HomePage from "./pages/homepage/homepage.comp";
+import ShopPage from "./pages/shop/shop.comp";
 
 import "./App.css";
 
@@ -7,7 +10,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+        </Switch>
       </div>
     );
   }
